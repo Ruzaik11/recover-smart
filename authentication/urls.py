fb9b1384a79980fa69b57e36ab1bb91d479400e1
Ruzaik <mnmruzaik@gmail.com>
2025-02-07 21:52:51 -0500

    returning server version

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import Authentication
+from django.http import HttpResponse
+import django
 
 urlpatterns = [
+    path('', lambda request: HttpResponse('Django: '+django.get_version())),
     path('api/auth/user/', Authentication.getUser),
     path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
